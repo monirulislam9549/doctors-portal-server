06ba436c1ac4c09e985311b358ec0e04689f7e0a
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-21 19:14:28 +0600

    booking implement

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
          *  app.delete('booking/:id') // delete specific one
          * */
 
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            console.log(query);
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }

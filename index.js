2ebc14601fd9b15e288a8c2c073690083fd4315d
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-21 10:06:16 +0600

    load to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
 
 require('dotenv').config();
 const app = express()
@@ -10,7 +11,73 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://doctors-admin:<password>@cluster0.i2xzc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i2xzc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors-portal').collection('services')
+        const bookingCollection = client.db('doctors-portal').collection('bookings')
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        // warning
+        // This is not the proper way to query.
+        // after learning more about mongodb.use aggregate lookup, pipeline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            // step:1 get all services
+            const services = await serviceCollection.find().toArray();
+
+            // step:2 get the booking of that day. output:[{}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step:3 for each service, find bookings for that services
+            services.forEach(service => {
+                // setep:4 find bookings for that service. output:[{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step:5 select slots for the service bookings:['', '', '', '',] 
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step:6 select those slots that are not in bookSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                // step:7 set available o slots to make it easier
+                service.slots = available;
+            })
+
+            res.send(services);
+        })
+
+        /** API Naming Convention
+         * app.get('/booking') // get all this booking in this collection or get more than one or by filter
+         * app.get('/booking/:id') // get a specific booking
+         * app.post('/booking') // add a new booking that means create a new operation
+         * app.patch('booking/:id') // specific one (patch means update)
+         *  app.delete('booking/:id') // delete specific one
+         * */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exist = await bookingCollection.findOne(query);
+            if (exist) {
+                return res.send({ success: false, booking: exist })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Doctors Portal!')
